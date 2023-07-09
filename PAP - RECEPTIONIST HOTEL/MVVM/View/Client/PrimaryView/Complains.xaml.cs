15f90ba2920aee7fbc964754efe1c0db65e2b1dc
Bagacinho-chan <95945441+bagacinho-chan@users.noreply.github.com>
2023-07-09 16:31:58 +0100

    a

--- PAP - RECEPTIONIST HOTEL/MVVM/View/Client/PrimaryView/Complains.xaml.cs
+++ PAP - RECEPTIONIST HOTEL/MVVM/View/Client/PrimaryView/Complains.xaml.cs
@@ -1,5 +1,7 @@
-﻿using System;
+﻿using PAP___RECEPTIONIST_HOTEL.Properties;
+using System;
 using System.Collections.Generic;
+using System.Data.SqlClient;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -15,14 +17,46 @@ using System.Windows.Shapes;
 
 namespace PAP___RECEPTIONIST_HOTEL.MVVM.View.Client.PrimaryView
 {
-    /// <summary>
-    /// Interaction logic for Complains.xaml
-    /// </summary>
     public partial class Complains : UserControl
     {
         public Complains()
         {
             InitializeComponent();
+        }
+
+        readonly SqlConnection con = new SqlConnection(Settings.Default.ConnectionString);
+
+        // VARIABLES
+        string data;
+
+        // OBJECTS
+
+        private void Complains_Loaded(object sender, RoutedEventArgs e)
+        {
+            // OPEN CONNECTION
+            con.Open();
+
+            data = "SELECT username FROM Users INNER JOIN Reservations " +
+                "ON Users.reservation_id = Reservations.id INNER JOIN Rooms " +
+                "ON Reservations.rooms_id = Rooms.id WHERE Rooms.n_room = @nRoom";
+
+            using (SqlCommand cmd = new SqlCommand(data, con))
+            {
+                cmd.Parameters.AddWithValue("@nRoom", ControlPanel.nRoom);
+
+                using (SqlDataReader reader = cmd.ExecuteReader())
+                {
+                    if (reader.Read())
+                    {
+                        usernameLabel.Content = reader["username"].ToString();
+                    }
+                }   
+            }
+
+            // CLOSE CONNECTION
+            con.Close();
+
+            nRoomLabel.Content = ControlPanel.nRoom;
         }
     }
 }
